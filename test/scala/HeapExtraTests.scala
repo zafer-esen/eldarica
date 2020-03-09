4987ee58139f4f3fbc9503f924493555f16fc046
Zafer Esen <zafer.esen@it.uu.se>
2020-03-09 15:27:28 +0100

    changes in extra tests

--- test/scala/HeapExtraTests.scala
+++ test/scala/HeapExtraTests.scala
@@ -17,7 +17,10 @@ class HeapTheoryExtraTests extends FlatSpec {
       ("WrappedInt", Heap.CtorSignature(List(("getInt",
         Heap.OtherSort(Sort.Integer))), ObjSort)),
       ("WrappedS", Heap.CtorSignature(List(("getS", StructSSort)), ObjSort)),
+      ("WrappedNode", Heap.CtorSignature(List(("getNode", StructSSort)), ObjSort)),
       ("struct_S", Heap.CtorSignature(List(("x", Heap.OtherSort(Sort.Integer))),
+        StructSSort)),
+      ("struct_Node", Heap.CtorSignature(List(("R", Heap.AddressCtorArgsSort)),
         StructSSort)),
       ("defObj", Heap.CtorSignature(List(), ObjSort))),
     defObjCtor)
@@ -30,17 +33,22 @@ class HeapTheoryExtraTests extends FlatSpec {
 
   val Seq(wrappedInt,
           wrappedS,
+          wrappedNode,
           struct_S,
+          struct_Node,
           defObj) = heap.ObjectADT.constructors
   val Seq(Seq(getInt),
           Seq(getS),
-          Seq(sel_x), _*) = heap.ObjectADT.selectors
+          Seq(getNode),
+          Seq(sel_x),
+          Seq(sel_R),_*) = heap.ObjectADT.selectors
 
   SimpleAPI.withProver(enableAssert = true) { pr : SimpleAPI =>
     import pr._
     import heap._
     val h = HeapSort.newConstant("h")
     val h1 = createConstant("h1", HeapSort)
+    val h2 = createConstant("h2", HeapSort)
     val ar = AllocResSort.newConstant("ar")
     val p1 =  createConstant("p1", AddressSort)
     val p2 =  createConstant("p2", AddressSort)
@@ -58,7 +66,7 @@ class HeapTheoryExtraTests extends FlatSpec {
       printOnlyOnFail = false)
     import priTests._
 
-    /*TestCase (
+   TestCase (
       "Reading back written value after chain allocation and a write.",
       CommonAssert(
         ar === alloc(newHeap(
@@ -90,26 +98,34 @@ class HeapTheoryExtraTests extends FlatSpec {
       CommonAssert(p1 === newAddr(alloc(emptyHeap(), wrappedS(struct_S(0))))),
       CommonAssert(p2 === p1),
       SatStep(p1 === p2)
-    )*/
+    )
     TestCase(
       "list-001-fail.c-2",
-      //SatStep(defObj() =/= wrappedS(struct_S(0)))
-      /*SatStep( h === emptyHeap() &&&
-        newHeap(alloc(emptyHeap(), defObj())) ===
-              newHeap(alloc(h, wrappedS(struct_S(0)))))*/
-      /*UnsatStep( h === emptyHeap() &&&
-               allocHeap(emptyHeap(), defObj()) ===
-               allocHeap(h, wrappedS(struct_S(0))))*/
-//      SatStep( h === newHeap(alloc(emptyHeap(), wrappedS(struct_S(42)))) &&&
- //              p1 === newAddr(alloc(emptyHeap(), wrappedS(struct_S(42)))) &&&
- //              o === read(h, p1))
-      SatStep(   h === newHeap(alloc(emptyHeap(), wrappedS(struct_S(0)))) &&&
-                 p1 === newAddr(alloc(emptyHeap(), wrappedS(struct_S(0)))) &&&
-                 //p2 === p1 &&&
-                 o === (getS(read(h, p1)))// &&&
-                 //p1 === p2
+      SatStep(
+        h1 === emptyHeap() &&&
+        h === newHeap(alloc(h1, wrappedS(struct_S(0)))) &&&
+        p1 === newAddr(alloc(h1, wrappedS(struct_S(0)))) &&&
+        p2 === p1 &&&
+        x === sel_x(getS(read(h, p2)))// &&&
       )
+    )
+    "All extra heap theory tests" should "pass" in {
+      assert(getRes._1 == getRes._2)
+    }*/
+    TestCase(
+      "list-004-fail.c",
+      SatStep(
+        h1 === newHeap(alloc(emptyHeap(), wrappedNode(struct_Node(0)))) &&&
+        p1 === newAddr(alloc(emptyHeap(), wrappedNode(struct_Node(0)))) &&&
+        h === newHeap(alloc(h1, p1)) &&&
+        h2 === write(h, p1, wrappedNode(struct_Node(p1)))
+        //write(h, )
+        //p1 === newAddr(alloc(h1, heap.nthAddr(0))) &&&
+        //write()
 
+        //sel_x(getS(read(h1,p1))) =/= sel_x(getS(read(h,p2)))
+        //x === sel_x(getS(read(h, p1)))// &&&
+      )
     )
     "All extra heap theory tests" should "pass" in {
       assert(getRes._1 == getRes._2)
